1dfd1040a6ba3e1cf64cbe4fca855390c864d94c
john-pels <oluseyiajeigbe@yahoo.com>
2019-08-13 13:52:35 +0100

    Added a function for delete button

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -6,9 +6,19 @@ const baseUrl = getBaseUrl();
 export function getUsers() {
     return get('users');
 }
+export function deleteUser(id) {
+    return del(`users/${id}`);
+}
 
 function get(url) {
     return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+//Can't call func delete since reserved word.
+function del(url) {
+    const request = new Request(baseUrl + url, {
+        method: 'DELETE'
+    });
 }
 
 function onSuccess(response) {
